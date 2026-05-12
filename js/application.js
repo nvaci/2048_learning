1cef535d2d425ceb34da87538edd0faf85e1f6b0
Nemanja Vaci <76957650+nvaci@users.noreply.github.com>
2026-05-12 12:35:36 +0100

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,4 +1,28 @@
-// Wait till the browser is ready to render the game (avoids glitches)
+// Read URL parameters
+const params = new URLSearchParams(window.location.search);
+
+// Participant ID
+const participantId =
+  params.get("participant_id") || crypto.randomUUID();
+
+// Session number
+const sessionNumber =
+  params.get("session") || "1";
+
+// Make globally accessible
+window.studyInfo = {
+  participantId,
+  sessionNumber
+};
+
+console.log(window.studyInfo);
+
+// Start game
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  new GameManager(
+    4,
+    KeyboardInputManager,
+    HTMLActuator,
+    LocalStorageManager
+  );
 });
